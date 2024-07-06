4951c229cd7f37ce2fd37339456a78a1cc5a9394
Rustam Uzairov <svrvt.zrv@gmail.com>
2024-07-06 18:42:56 +0300

    ...

--- bin/script.sh
+++ bin/script.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-# list="bash zsh gitconfig nvim vim ranger tmux vifm xplr zathura"
-list="alacritty rofi environment"
+# list="bash zsh gitconfig nvim vim ranger tmux vifm xplr zathura alacritty rofi environment"
+list="$(vcsh list | sed 's|vc_||g' | tr '\n' ' ')"
 
+# file=vcsh-default
 for l in $list; do
-	vcsh init vc_"$l"
-	gh repo create vc_"$l" --public -y
-	vcsh run vc_"$l" git remote add origin git@github.com:svrvt/vc_"$l"
+	# if grep -q vc_"$l" ~/.config/mr/available.d/$file; then
+	#   grep vc_"$l" ~/.config/mr/available.d/$file >~/.config/mr/available.d/vc_"$l".vcsh
+	#   echo -e '\n# vi: ft=ini syntax=sh' | tee -a /home/ru/.config/mr/available.d/vc_"$l".vcsh
+	# fi
+
+	if [ "$l" != vc ]; then
+    # vcsh upgrade vc_"$l"
+    # vcsh write-gitignore vc_"$l"
+		# vc "$l" git add -f "/home/ru/.config/mr/available.d/vc_$l.vcsh"
+		vc "$l" git commit -m "add available.d/vc_$l"
+		# echo -e "$HOME/.config/mr/available.d/vc_$l.vcsh"
+	fi
+	# vcsh init vc_"$l"
+	# gh repo create vc_"$l" --public -y
+	# vcsh run vc_"$l" git remote add origin git@github.com:svrvt/vc_"$l"
+	#
 	# vcsh run vc_"$l" git push --set-upstream origin main
 done
 
