53e805c984d79d601c983888d8c99e16a9a0b97e
Rustam Uzairov <svrvt.zrv@gmail.com>
2025-05-25 00:25:59 +0300

    saturdays multicommit

--- bin/setup/systemctldisable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-systemd-analyze critical-chain
-systemd-analyze blame
-systemd-analyze
-# sudo systemctl disable systemd-networkd-wait-online.service && sudo systemctl mask systemd-networkd-wait-online.service
